2237cf1fcf3324cf3280cbf011fef51844ea6040
Samuel <sbabinski@outlook.com>
2021-12-18 00:25:53 -0300

    Finished the javascript, added the functions to reset the game and to better log the plays

--- script.js
+++ script.js
@@ -1,49 +1,51 @@
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+const newGameButton = document.getElementById('newGame');
 
-const round1 = document.getElementById('li1');
-const round2 = document.getElementById('li2');
-const round3 = document.getElementById('li3');
-const round4 = document.getElementById('li4');
-const round5 = document.getElementById('li5');
+let round = 0;
+let playerWinCounter = 0;
+let computerWinCounter = 0;
 
 rockButton.addEventListener('click', game);
 paperButton.addEventListener('click', game);
 scissorsButton.addEventListener('click', game);
+newGameButton.addEventListener('click', resetGame);
 
 function logPlays(round, playedResult, computerSelection) {
-    document.getElementById(round).innerHTML = `Computer choosed ${computerSelection}.`;
+    document.getElementById(round).innerHTML = 'Turn ' + round.charAt(2) +' - ';
+    document.getElementById(round).innerHTML += `Computer choosed ${computerSelection}.`;
     document.getElementById(round).innerHTML += ` ${playedResult}`;
 }
 
 function game(e) {
-    let playerWinCounter = 0;
-    let computerWinCounter = 0;
-    for (let round = 0; round < 5; round++) {
-        let message = '';
-        const computerSelected = computerSelection();
-        switch (playerSelection(e)) {
-            case 'rock':
-                message = rockAgainst(computerSelected);
-            case 'paper':
-                message = paperAgainst(computerSelected);
-            case 'scissors':
-                message = scissorsAgainst(computerSelected);
-        }
-        if (message.includes("won")) {
-            playerWinCounter++;
-            logPlays("li" + (round + 1), message, computerSelected);
-        }
-        if (message.includes("lost")) {
-            computerWinCounter++;
-            logPlays("li" + (round + 1), message, computerSelected);
-        }
-        if (message.includes("tie")) {
-            round--;
-            window.alert(message);
-        }
+    let message = '';
+    const computerSelected = computerSelection();
+    switch (playerSelection(e)) {
+        case 'rock':
+            message = rockAgainst(computerSelected);
+            break;
+        case 'paper':
+            message = paperAgainst(computerSelected);
+            break;
+        case 'scissors':
+            message = scissorsAgainst(computerSelected);
+            break;
     }
+    if (message.includes("won")) {
+        playerWinCounter++;
+        logPlays("li" + (round + 1), message, computerSelected);
+    }
+    if (message.includes("lost")) {
+        computerWinCounter++;
+        logPlays("li" + (round + 1), message, computerSelected);
+    }
+    if (message.includes("tie")) {
+        logPlays("li" + (round + 1), message + ' This round will be played again.', computerSelected);
+        round--;
+    }
+    round += 1;
+    gameEnd();
 }
 
 function playerSelection(e) {
@@ -58,16 +60,13 @@ function computerSelection() {
 function rockAgainst(computerSelection) {
     switch (computerSelection) {
         case "Rock":
-            return ("It's a tie")
+            return ("It's a tie.")
             break;
         case "Paper":
-            return ("You lost! Paper beats Rock")
+            return ("You lost! Paper beats Rock.")
             break;
         case "Scissors":
-            return ("You won! Rock beats Scissors")
-            break;
-        default:
-            return ("Input not valid")
+            return ("You won! Rock beats Scissors.")
             break;
     }
 }
@@ -75,16 +74,13 @@ function rockAgainst(computerSelection) {
 function paperAgainst(computerSelection) {
     switch (computerSelection) {
         case "Rock":
-            return ("You won! Paper beats Rock")
+            return ("You won! Paper beats Rock.")
             break;
         case "Paper":
-            return ("It's a tie")
+            return ("It's a tie.")
             break;
         case "Scissors":
-            return ("You Lost! Scissors beats Paper")
-            break;
-        default:
-            return ("Input not valid")
+            return ("You lost! Scissors beats Paper.")
             break;
     }
 }
@@ -92,26 +88,45 @@ function paperAgainst(computerSelection) {
 function scissorsAgainst(computerSelection) {
     switch (computerSelection) {
         case "Rock":
-            return ("You lost! Rock beats Scissors")
+            return ("You lost! Rock beats Scissors.")
             break;
         case "Paper":
-            return ("You won! Scissors beats Paper")
+            return ("You won! Scissors beats Paper.")
             break;
         case "Scissors":
-            return ("It's a tie")
-            break;
-        default:
-            return ("Input not valid")
+            return ("It's a tie.")
             break;
     }
 }
 
-function gameEnd(playerCounter, computerCounter) {
-    if (playerWinCounter > computerWinCounter) {
-        window.alert("You win! " + playerWinCounter + " x " + computerWinCounter + " for you.");
-    } else if (playerWinCounter < computerWinCounter) {
-        window.alert("You lost! " + playerWinCounter + " x " + computerWinCounter + " for the computer.");
-    } else {
-        window.alert("It was a tie! " + playerWinCounter + " x " + computerWinCounter + " for no one.")
+function gameEnd() {
+    if (playerWinCounter == 3) {
+        document.getElementById('result').innerHTML = 'Result: You won!';
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        newGameButton.disabled = false;
     }
+    if (computerWinCounter == 3) {
+        document.getElementById('result').innerHTML = 'Result: You lost!';
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        newGameButton.disabled = false;
+    }
+
+}
+
+function resetGame() {
+    round = 0;
+    playerWinCounter = 0;
+    computerWinCounter = 0;
+    document.querySelectorAll('li').forEach(element => {
+        element.innerHTML = '';
+    });
+    document.getElementById('result').innerHTML = 'Result:'
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    newGameButton.disabled = true;
 }
